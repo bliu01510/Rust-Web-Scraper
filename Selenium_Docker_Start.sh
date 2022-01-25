6926c912cda6c0bfb3b82718f616ce74ebb4b3cd
bliu01510 <liuy01510.work@gmail.com>
2022-01-25 00:56:47 +0000

    Removed the Startup_Selenium bash script.

--- Selenium_Docker_Start.sh
+++
@@ -1,4 +0,0 @@
-# sudo docker run --rm -d -p 4444:4444 --name selenium-server -e START_XVFB=false -v /dev/shm:/dev/shm --shm-size="2g" selenium/standalone-chrome:4.1.1-20211217
-sudo docker run --rm -d -p 4444:4444 --name selenium-server -v /dev/shm:/dev/shm --shm-size="2g" selenium/standalone-chrome:4.1.1-20211217
-echo "Selenium Docker started!"
-sudo docker ps
